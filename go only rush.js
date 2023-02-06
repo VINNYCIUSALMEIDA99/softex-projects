4763ef924089a01365a373102cfb9ea030979ed8
VINNYCIUSALMEIDA99 <110493810+VINNYCIUSALMEIDA99@users.noreply.github.com>
2023-02-06 15:57:52 -0300

    Add files via upload

---
+++ go only rush.js
@@ -0,0 +1,32 @@
+
+class Pessoa {
+    constructor(nome, sobrenome) {
+      this.nome = nome;
+      this.sobrenome = sobrenome;
+    }
+  
+    get nomeDaPessoa() {
+      return this.nome;
+    }
+  
+    get sobrenomeDaPessoa() {
+      return this.sobrenome;
+    }
+  
+    set mudarNomePessoa(nomePessoa) {
+      return (this.nome = nomePessoa);
+    }
+  
+    set mudarSobrenomePessoa(sobrenomePessoa) {
+      return (this.sobrenome = sobrenomePessoa);
+    }
+  }
+  
+  let pessoaExemplo = new Pessoa("vinny", "almeida");
+  console.log(pessoaExemplo.nome);
+  console.log(pessoaExemplo.sobrenome);
+  
+  pessoaExemplo.mudarNomePessoa = "Zezinho";
+  pessoaExemplo.mudarSobrenomePessoa = "Shaolin";
+  console.log(pessoaExemplo.nome);
+  console.log(pessoaExemplo.sobrenome);
